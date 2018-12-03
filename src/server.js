7e4cdb389a297d7e5f0d58034126c4ab4d8b9278
David Chambers <40244269+dlchambersjr@users.noreply.github.com>
2018-12-02 18:39:54 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const cors = require('.cors');
+const cors = require('cors');
 
 // local middleware will load here
 const router = require('./routes.js');
